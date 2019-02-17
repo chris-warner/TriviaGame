5e4125bb7293c571fcd563caa41828cbe7a6a709
Chris Warner <chriswarner@Chriss-MacBook-Pro.local>
2019-02-17 15:31:58 -0500

    code now updates displayDiv to show wins and losses when the timer runs out

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,8 +9,11 @@ var losses = 0;
 // }, 3000);
 // countDown(3000);
 
+$(".questionDiv").hide();
+
 $("#btnNewgame").click(function() {
     newGame();
+    $(".questionDiv").show();
     $(this).hide();
 });
 
@@ -23,18 +26,20 @@ function countDown(i) {
 
 
 function newGame() {
-    countDown(3);
+    countDown(30);
 }
 
 //If timer is 0, call this function. When the game is finished, this concluded the score and handles the reset.
 function timeup() {
     alert("time is up!");
+    //Hide the questions
+    $(".questionDiv").hide();
     calculateScore();
 }
 
 function calculateScore() {
     $(function() {
-        //Question 1
+        //Calculate questions correct and incorrect
         if ($('input[name=question1]:checked').val() === "Christina Aguilera") {
             wins++;
         } else {
@@ -50,11 +55,22 @@ function calculateScore() {
         } else {
             losses++;
         }
+        if ($('input[name=question4]:checked').val() === "Eye of the Tiger") {
+            wins++;
+        } else {
+            losses++;
+        }
+        if ($('input[name=question5]:checked').val() === "Madonna") {
+            wins++;
+        } else {
+            losses++;
+        }
         displayScore();
     });
 }
 
 function displayScore() {
-    alert($('input[name=question1]:checked').val());
+    //alert($('input[name=question1]:checked').val());
     alert("Correct: " + wins + " " + "Incorrect: " + losses);
+    document.getElementById("displayDiv").innerHTML = "Answeres correct: " + wins + " " + "Incorrect: " + losses;
 }
